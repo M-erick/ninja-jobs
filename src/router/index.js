37f66a09590b80b9969626fa6df2dc8402e5e0e0
eric <61746940+M-erick@users.noreply.github.com>
2022-01-26 12:02:38 +0300

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+ 
+import Jobs from '../views/Jobs.vue'
+
+
+//registering routes
+
+const routes = [
+
+    
+
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
+ 
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        
+ 
+    </div>
+</template>
+<script>
+export default {
+    name :'Home',
+    setup() {
+        let name = 'erick'
+        let age = 30
+        const handleClick = () => {
+            console.log('you clicked me')
+        }
+        const p = ref(null)
+
+
+        return{name,age,handleClick}
+        
+    }
+   
+    
+}
+</script>
+<style >
+    
+</style>
